86dc0fe5bef0c6ba5e9e9b5532cecd89fcbe3c84
sk sabur ali <sabur.ali@codeclouds.in>
2025-11-27 14:49:51 +0530

    First commit

---
+++ src/main/java/com/NoteApp/Note/DTO/NoteResponse.java
@@ -0,0 +1,16 @@
+package com.NoteApp.Note.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+//test
+//test1
+public class NoteResponse {
+    private int noteId;
+    private String noteTitle;
+    private String noteDesc;
+    private String noteDate;
+    private String noteTime;
+}
